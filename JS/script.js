6285bfc3b8bd84f4eb261d7889f3ab9a0a557143
Samvel <manukyansamvel93@gmail.com>
2021-05-26 21:46:01 +0400

    first commit

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second = 2;
+console.log(second);
+
+let persone = {
+    name: "Arsen",
+    age: 26,
+    city: "Ekaterinburg",
+    profession: "karate"
+};
+
+console.log(persone.profession);
+
+let arr = ['lendrush.jpg', 'owl.png', 'nature.svg']
+
+console.log(arr[1]);
+
+// let answer = confirm("are you well?")
+
+// console.log(answer);
+
+let answer = prompt("are you 18 year?", "");
+
+console.log(answer);
